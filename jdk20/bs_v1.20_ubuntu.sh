b08181a869faf4928b85fa62ba513c3c5c29e322
sepsallao <sallaojoseph@hotmail.com>
2023-03-27 02:58:46 +0800

    added jdk 20

--- jdk20/bs_v1.20_ubuntu.sh
+++ jdk20/bs_v1.20_ubuntu.sh
@@ -8,16 +8,18 @@ echo "Version 0.1"
 
 apt-get -y update
 
-# Download JDK 19
-wget https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.deb
+# Download JDK 19 from oracle
+# wget https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.deb
+wget https://download.oracle.com/java/20/latest/jdk-20_linux-x64_bin.deb
 
-# Install JDK 19
-apt-get -y install ./jdk-19_linux-x64_bin.deb
+# Install JDK 20 with Deb file
+# apt-get -y install ./jdk-19_linux-x64_bin.deb
+sudo apt-get -y install ./jdk-19_linux-x64_bin.deb
 
 # Setup Java
-sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk-19/bin/java 1
-sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/jdk-19/bin/javac 1
-sudo update-alternatives --install /usr/bin/jar jar /usr/lib/jvm/jdk-19/bin/jar 1
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk-20/bin/java 1
+sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/jdk-20/bin/javac 1
+sudo update-alternatives --install /usr/bin/jar jar /usr/lib/jvm/jdk-20/bin/jar 1
 sudo update-alternatives --config java
 sudo update-alternatives --config javac
 sudo update-alternatives --config jar
@@ -26,8 +28,8 @@ sudo update-alternatives --config jar
 java --version
 javac --version
 
-# Link jdk-19 to $JAVA_HOME 
-export JAVA_HOME=/usr/lib/jvm/jdk-19
+# Link jdk-19 to $JAVA_HOME
+sudo export JAVA_HOME=/usr/lib/jvm/jdk-20
 
 # Setting up the JDK's Script
 sudo cat jdk.sh > /etc/profile.d/jdk.sh
@@ -48,8 +50,14 @@ apt-get install -y libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-d
 echo "Starting to clone from OpenJDK repository:"
 git clone https://github.com/openjdk/jdk.git
 
-# Running configure to build
-cd jdk/
+# To pull for building a certain version of jdk's source code:
+#```bash
+# $ git checkout tags/jdk-19+36
+#```
+git checkout tags/jdk-20+34
+
+# Running configure to buildf
+cd jdk/f
 
 echo "our location:"
 pwd
